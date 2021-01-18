74f445ed9cb2144492c18e353ed5251d8ef346d1
or kilim <48524233+orkilim@users.noreply.github.com>
2021-01-18 03:17:17 +0200

    search works

--- src/Components/MainComponents/Search.js
+++ src/Components/MainComponents/Search.js
@@ -1,52 +1,51 @@
 import axios from 'axios'
 import * as React from 'react'
 import Navbar from '../RepeatingComponents/Navbar'
-import { Input } from '@material-ui/core';
+import { Button, Input } from '@material-ui/core';
 
 let dogsImagesArray = []
-let fullArray = false
+let key=0
 
 const showImages = (imageLink) => {
-    if (fullArray) {
         return (
-            <img style={{width:100,height:100}} src={imageLink} alt='dog image' />
+            <img style={{ width: 100, height: 100 }} key={key++} src={imageLink} alt='dog image' />
         )
-    }
-    else{
-        return <text>problem with playing fetch...ing the images</text>
-    }
+    
+    
 }
 
 
 const Search = () => {
 
     const [dogBreed, setBreed] = React.useState('')
-
-    axios({
-        method: "GET",
-        url: `https://dog.ceo/api/breed/${dogBreed}/images`
-
-    })
-        .then((responseJson) => {
-                fullArray = true
-                dogsImagesArray = responseJson.data.message
-        })
-        .catch((err)=>{
-            if(err)
-                console.log('error is:\n'+err)
-        })
-
-
+    const [fullArray,setFullArray]=React.useState(false)
 
     return (
 
         <div>
-            <Input type='text' placeholder="dog breed..." onChange={(event) => {setBreed(event.currentTarget.value) }} />
-            <div style={{justifyContent:'flex-start'}}>
+            <Input type='text' placeholder="dog breed..." onChange={(event) => { setBreed(event.currentTarget.value) }} />
+            <Button onClick={async () => {
+
+                await axios({
+                    method: "GET",
+                    url: `https://dog.ceo/api/breed/${dogBreed}/images`
+
+                    })
+                    .then((responseJson) => {
+                        if (responseJson.data.status === 'success') {
+                            dogsImagesArray = responseJson.data.message
+                            setFullArray(true)
+                        }
+                    })
+                    .catch((err) => {
+                        if (err)
+                            console.log('error is:\n' + err)
+                    })
+
+            }}>SEARCH</Button>
+            <div>
             {
-
                 dogsImagesArray.map(showImages)
-
             }
             </div>
             <Navbar />
@@ -58,37 +57,3 @@ export default Search
 
 
 
-
-/*
-{
-                axios.get(`/dog.ceo/api/breed/${dogBreed}/images`)
-                    .then((responseJson) => {
-                        if (responseJson.status !== 'success')
-                            return <text>a problem has occured</text>
-                        else {
-                            responseJson.message.map(showImages())
-                        }
-                    })
-            }
-            <Navbar />
-*/
-
-
-/*
-{
-                axios({
-                    method:"GET",
-                    url:`/dog.ceo/api/breed/${dogBreed}/images`
-
-                })
-                .then((responseJson)=>{
-                    if(responseJson.data.status!=='success')
-                    {
-                        return <text>problem with fetching...the images</text>
-                    }
-                    else{
-                        responseJson.data.message.map(showImages())
-                    }
-                })
-            }
-             */
