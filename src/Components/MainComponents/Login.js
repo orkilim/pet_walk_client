4d88790e1a757ba0c35891d7c5b6ac6b881a9750
shani24levi <48565585+shani24levi@users.noreply.github.com>
2021-01-24 05:57:19 +0200

    login validation

--- src/Components/MainComponents/Login.js
+++ src/Components/MainComponents/Login.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 import '../../App.css';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavLink, } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Joi from 'joi-browser';
 
 
@@ -25,9 +25,6 @@ const Login = () => {
             email: email,
             pass: pass,
         }
-
-        console.log(our_data);
-
         let errors = {};
 
         //chack valid form inputs
