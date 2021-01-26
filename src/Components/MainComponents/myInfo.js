890bb70550a1cfe282fd62c32ec75fefda3ebd46
shani24levi <48565585+shani24levi@users.noreply.github.com>
2021-01-26 14:37:20 +0200

    myInfo adding and chart com

--- src/Components/MainComponents/myInfo.js
+++ src/Components/MainComponents/myInfo.js
@@ -5,11 +5,16 @@ import ArrowBackIosRoundedIcon from '@material-ui/icons/ArrowBackIosRounded';
 import ArrowForwardIosRoundedIcon from '@material-ui/icons/ArrowForwardIosRounded';
 import Navbar from '../RepeatingComponents/Navbar';
 import Loading from '../RepeatingComponents/Loading';
+import { Pie } from 'react-chartjs-2';
+import Chart from './Chart';
+
+
 
 const MyInfo = () => {
     const [myPets, setMyPets] = React.useState([]);
     const [data, setData] = React.useState(false); // data retrieved from the server(y\n)
     const [petIndex, setPetIndex] = React.useState(0); // index from myPets array 
+    const [chart1, setChart1] = React.useState({});
 
     React.useEffect(() => {
         console.log(data);
@@ -38,21 +43,37 @@ const MyInfo = () => {
     //Click and render the pet in the array :
     const handleClick = () => {
         if (petIndex == 0) {
-            setPetIndex(myPets.data.length-1);
+            setPetIndex(myPets.data.length - 1);
         }
         if (petIndex > 0) {
-            setPetIndex(petIndex-1);
+            setPetIndex(petIndex - 1);
         }
     }
 
     const handleClickRight = () => {
-        if (petIndex == myPets.data.length-1) {
+        if (petIndex == myPets.data.length - 1) {
             setPetIndex(0);
         }
-        if (petIndex >= 0 && petIndex < myPets.data.length-1) {
-            setPetIndex(petIndex+1);
+        if (petIndex >= 0 && petIndex < myPets.data.length - 1) {
+            setPetIndex(petIndex + 1);
         }
     }
+
+    // const ChartData = () => {
+    //     let charData= {
+    //         datasets: [{
+    //             data:[myPets.data[petIndex].age,20-myPets.data[petIndex].age],
+    //             backgroundColor: ['rgb(110, 168, 255)','rgb(255, 255, 255)']
+    //         }]
+    //     }
+    //     setChart1(charData);
+    //     return(
+    //         <Pie
+    //         //data= {chart1}
+    //     />
+    //     )
+    // }
+
 
     const showMyPets = () => {
         if (myPets.length == 0) {
@@ -85,11 +106,51 @@ const MyInfo = () => {
                             <h5 className="pb-2 pl-2" style={{ color: '#727377' }}> age {myPets.data[petIndex].age}</h5>
                         </div>
                         <ArrowForwardIosRoundedIcon onClick={handleClickRight} className="align-self-center" style={{ fontSize: '200%', color: '#6EA8FF' }}></ArrowForwardIosRoundedIcon>
-
                     </div>
                 </div>
+                <h3 className="mt-4 pb-2">State</h3>
+                {/* dayly plan section */}
+                <div className='row p-3 justify-content-between btn_section mb-3' style={{ boxShadow: 'none' }}>
+                    <div class="col-8 align-self-center p-2">
+                        <h4><strong>Today's Plans</strong></h4>
+                        <h5>{myPets.data[petIndex].age} tasks completed</h5>
+                    </div>
+                    <div class='col-4 align-self-center text-center'>
+                        {/* {ChartData()} */}
+                        <Chart mydata={myPets.data[petIndex].age} />
+                    </div>
+                </div>
+                {/*End of dayly plan section */}
 
+                {/* Energy avaliable section */}
+                <div className='row p-3 justify-content-between btn_section mb-3' style={{ boxShadow: 'none' }}>
+                    <div class="col-8 align-self-center p-2">
+                        <h4><strong>Energy avaliable</strong></h4>
+                        <h5>{myPets.data[petIndex].age} tasks completed</h5>
+                    </div>
+                    <div class='col-4 align-self-center text-center'>
+                        {/* {ChartData()} */}
+                        <Chart mydata={myPets.data[petIndex].age} />
+                    </div>
+                </div>
+                {/*End of Energy avaliable section */}
 
+                {/* EDaily food habits section */}
+                <div className='row p-3 justify-content-between btn_section mb-3' style={{ boxShadow: 'none' }}>
+                    <div class="col-8 align-self-center p-2">
+                        <h4><strong>Daily food habits</strong></h4>
+                        <h5>{myPets.data[petIndex].age} tasks completed</h5>
+                    </div>
+                    <div class='col-4 align-self-center text-center'>
+                        {/* {ChartData()} */}
+                        <Chart mydata={myPets.data[petIndex].age} />
+                    </div>
+                </div>
+                {/*End of Daily food habits section */}
+
+                <div class='col-auto text-center' style={{marginBottom: '100px'}}>
+                    <button class="btnStyle btn-lg btns_blue mt-4 w-75">Start a Play</button>
+                </div>
             </React.Fragment>
         )
     }
